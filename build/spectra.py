39e126bdcb72f786c7004182fc0f79f644077924
Ben Derieg <bderieg@deriegfamily.com>
2022-06-16 15:59:05 -0500

    Modified the python script to show multiple spectra overlaid, including initial

--- build/spectra.py
+++ build/spectra.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # ***Import data***
 print("Importing data . . . ")
-data = pd.read_csv("secondary_particle_info.csv", names=["particle","counter","E_k"], nrows=5000)
+data = pd.read_csv("secondary_particle_info.csv", names=["particle","counter","E_k"], nrows=100000)
 
 # ***Get total particle counts (at each energy)***
 # Initialize with beam configuration
@@ -34,11 +34,42 @@ for key in particleTracker:
         for i in range(int(particleTracker[key])):
             particleEnergies[key[0]].append(key[1])
 
+# ***Create original spectra***
+print("Creating beam spectra . . . ")
+originalBeamSpectra = {('alpha', 400) : 1e6,  # Keys are a tuple: (particle, E_k)
+                  ('proton', 100) : 1e6,  # Values are the count
+                  ('Be9', 900) : 1e6}
+
+originalEnergies = {}
+for key in originalBeamSpectra:
+    if key[1] != 0:
+        # Create key if it doesn't exist
+        if key[0] not in originalEnergies:
+            originalEnergies[key[0]] = []
+        # Add the energy for as many particles as exist
+        for i in range(int(originalBeamSpectra[key])):
+            originalEnergies[key[0]].append(key[1])
+
 # ***Create histograms***
 print("Creating histograms . . . ")
-plt.subplot(111)
-plt.hist(particleEnergies['proton'],
-        bins=np.geomspace(min(particleEnergies['proton']), min(particleEnergies['proton']), 100))
+
+xlims = [1e-3, 1e3]
+ylims = [0.1, 5.0]
+beamParticles = ['proton', 'alpha', 'Be9']
+secondaryParticles = ['neutron', 'e-']
+
+plt.subplot(121)
+for name in beamParticles:
+    plt.hist(pd.Series(originalEnergies[name]),histtype='step',
+            bins=np.geomspace(pd.Series(originalEnergies[name]).min(), pd.Series(originalEnergies[name]).max(), 50))
 plt.xscale('log')
-plt.yscale('log')
-plt.savefig('gamma_1milruns_hist.jpg')
+# plt.yscale('log')
+
+plt.subplot(122)
+for name in secondaryParticles:
+    plt.hist(pd.Series(particleEnergies[name]),histtype='step',
+            bins=np.geomspace(pd.Series(particleEnergies[name]).min(), pd.Series(particleEnergies[name]).max(), 50))
+plt.xscale('log')
+# plt.yscale('log')
+
+plt.savefig('spectra.jpg')
