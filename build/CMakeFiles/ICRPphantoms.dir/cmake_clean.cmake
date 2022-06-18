f57ea9ac8cf21a501611cb574a826e76b4723e34
Ben Derieg <bderieg@deriegfamily.com>
2022-06-17 22:05:19 -0500

    Updated ignore

--- build/CMakeFiles/ICRPphantoms.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ICRPphantoms"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ICRPphantoms.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
